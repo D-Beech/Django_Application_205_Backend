1c2dc0653137050376a0135b2db17054f68fb435
d-beech <datubeechmusic@gmail.com>
2024-10-08 10:56:01 +1300

    Webscraper Functions, parsing product cards for name and price (new world)

--- Webscrapers/scrape.py
+++ Webscrapers/scrape.py
@@ -12,7 +12,12 @@ source = source_html(new_world)
 
 soup = BeautifulSoup(source, 'lxml')
 
-for match in soup.find_all('p', class_='_1afq4wy7 w6tzb3l w6tzb31r'):
-    title = match.text
-    print(title)
+# for match in soup.find_all('p', class_='_1afq4wy7 w6tzb3l w6tzb31r'):
+#     title = match.text
+#     print(title)
 
+for match in soup.find_all('div', class_="_1afq4wy0"):
+    title = match.find('p', class_='_1afq4wy7 w6tzb3l w6tzb31r').text
+    dollars = match.find('p', class_='whm0tf1 _1xxz4ve0 _1xxz4ve2').text
+    cents = match.find('p', class_='whm0tf3 _1xxz4ve4 _1xxz4ve6').text
+    print(f"Product Name: {title}\n Price: ${dollars}.{cents}")
