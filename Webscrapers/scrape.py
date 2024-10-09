c3782920ad3120a4d9824b7295597f8667562b36
d-beech <datubeechmusic@gmail.com>
2024-10-10 10:47:46 +1300

    disabled scraping functions that trigger cloudflare bot protections. Added url table update automation functions

--- Webscrapers/scrape.py
+++ Webscrapers/scrape.py
@@ -17,7 +17,7 @@ categories = ["/fresh-foods-and-bakery",
 
 
 
-#Beautiful Soup House Keeping
+#Start of Beautiful Soup House Keeping
 def source_html(url):
     dr = webdriver.Chrome()
     dr.get(url)
@@ -25,27 +25,12 @@ def source_html(url):
 
 def cook_soup(url):
     return BeautifulSoup(source_html(url), 'lxml')
+#End of Beautiful Soup House Keeping
 
 
-#Workhorse Function, currently writing rows to CVC file, need to refactor
-def messy_function(url):
-    soup = cook_soup(url)
-
-    for match in soup.find_all('div', class_="_1afq4wy0"):
-
-        title = match.find('p', class_='_1afq4wy7 w6tzb3l w6tzb31r').text.strip()
-        dollars = match.find('p', class_='whm0tf1 _1xxz4ve0 _1xxz4ve2').text.strip()
-        cents = match.find('p', class_='whm0tf3 _1xxz4ve4 _1xxz4ve6').text.strip()
-        price = int(dollars + cents) / 100
-        img = match.find('img', class_='_14gbuuw2')
-        img_src = img['src']
-        link_a = match.find('a', class_="_1afq4wy2")
-        link_to_product = link_a['href']
-        csv_writer.writerow([title, dollars, cents] )
-
-
+#Start of url update automation
 #This function returns the max for number of pages for a catergory by finding the final a tag value for the page nav
-#It is not always accurate (sometimes returns value less than actual max, maybe 10%-20% of the time)
+#It is not always accurate (sometimes returns value less than actual max, maybe 10% of the time, it is fine for this univeristy application)
 def last_page(soup):
     num = 0
     for match in soup.find_all('a', class_='_10vhnom2 _10vhnom1'):
@@ -63,22 +48,56 @@ def page_number_ranges():
         ranges.append(max)
     return ranges
 
+#This function writes all page urls in csv file
+def update_new_world_urls():
+    ranges = page_number_ranges()
+
+    urls_file = open('new_world_urls.csv','w', newline='')
+    csv_writer = csv.writer(urls_file)
+
+    for i in range(len(ranges)):
+        for page in range(0, ranges[i]):
+            url = new_world + categories[i] + "?pg=" + str(page)
+            print(url)
+            csv_writer.writerow([url])
+
+    urls_file.close()
+#End of url update automation
+
+
+
+
+#Workhorse Function, currently writing rows to CVC file, need to refactor
+#Running this code as is will trigger cloudflare warnings at New World derver end
+def messy_function(url):
+    soup = cook_soup(url)
+
+    for match in soup.find_all('div', class_="_1afq4wy0"):
+
+        title = match.find('p', class_='_1afq4wy7 w6tzb3l w6tzb31r').text.strip()
+        dollars = match.find('p', class_='whm0tf1 _1xxz4ve0 _1xxz4ve2').text.strip()
+        cents = match.find('p', class_='whm0tf3 _1xxz4ve4 _1xxz4ve6').text.strip()
+        price = int(dollars + cents) / 100
+        img = match.find('img', class_='_14gbuuw2')
+        img_src = img['src']
+        link_a = match.find('a', class_="_1afq4wy2")
+        link_to_product = link_a['href']
+        csv_writer.writerow([title, dollars, cents] )
+
+
+
+
 
 #Testing Code is Below
-ranges = page_number_ranges()
-
-csv_file = open('scrape_data.csv','w', newline='') 
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['name', 'dollars','cents'])
 
 
-for i in range(len(ranges)):
-    for page in range(0, ranges[i]):
-        url = new_world + categories[i] + "?pg=" + str(page)
-        print(url)
-        messy_function(url)
+# csv_file = open('scrape_data.csv','w', newline='') 
+# csv_writer = csv.writer(csv_file)
+# csv_writer.writerow(['name', 'dollars','cents'])
 
-csv_file.close()
+
+
+# csv_file.close()
 
 # reader = csv.DictReader(open('scrape_data.csv'))
 # for row in reader:
